53555290fb3ca3f163e7660f4c42dbc473943cf8
Rithik Jain <rithik.jain3006@gmail.com>
2020-04-30 13:34:58 +0530

    Made ui changes to landing page

--- lib/pages/landing/landing_page.dart
+++ lib/pages/landing/landing_page.dart
@@ -16,20 +16,68 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Swiper(
-        itemBuilder: (BuildContext context, int index) {
-          return pages[index];
-        },
-        itemCount: pages.length,
-        loop: false,
-        scrollDirection: Axis.horizontal,
-        pagination: SwiperPagination(
-          builder: DotSwiperPaginationBuilder(
-            color: Colors.black,
-            activeColor: Colors.black,
-            size: 5,
-            activeSize: 10,
-          ),
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: double.infinity,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFFC6C6C),
+                      ),
+                      child: Image.asset(
+                        "assets/images/bg.png",
+                        fit: BoxFit.fill,
+                        color: Color(0xFFDB162F),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: double.infinity,
+                      width: double.infinity,
+                      transform: Matrix4.translationValues(0, -20, 0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  return pages[index];
+                },
+                itemCount: pages.length,
+                loop: false,
+                scrollDirection: Axis.horizontal,
+                pagination: SwiperPagination(
+                  builder: DotSwiperPaginationBuilder(
+                    color: Colors.black,
+                    activeColor: Colors.black,
+                    size: 5,
+                    activeSize: 10,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/pages/landing/landing_page_2.dart
+++ lib/pages/landing/landing_page_2.dart
@@ -3,85 +3,63 @@ import 'package:flutter/material.dart';
 class LandingPage2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Stack(
-              alignment: Alignment.topCenter,
-              children: <Widget>[
-                Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFC6C6C),
-                  ),
-                  child: Image.asset(
-                    "assets/images/bg.png",
-                    fit: BoxFit.fill,
-                    color: Color(0xFFDB162F),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    child: Image.asset("assets/images/chef2.png"),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              height: double.infinity,
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 16,
-                    ),
-                    child: Text(
-                      "Share your recipes",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 16,
-                    ),
-                    child: Text(
-                      "Share your recipes with others,\neasily",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ],
-              ),
-              transform: Matrix4.translationValues(0, -20, 0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  child: Image.asset("assets/images/chef2.png"),
                 ),
               ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 1,
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 16,
+                      ),
+                      child: Text(
+                        "Share your recipes",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 16,
+                      ),
+                      child: Text(
+                        "Share your recipes with others,\neasily",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

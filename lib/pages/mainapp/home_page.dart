37d5c13aa3f19eb9c3ac23de058e59e1891225cb
Rithik Jain <rithik.jain3006@gmail.com>
2020-05-11 13:33:32 +0530

    Added an appbar to home page

--- lib/pages/mainapp/home_page.dart
+++ lib/pages/mainapp/home_page.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            SvgPicture.asset("assets/images/logo.svg"),
+            Icon(Icons.star_border, color: Colors.black,),
+          ],
+        ),
+      ),
       body: Center(
         child: Text('HomePage'),
       ),
